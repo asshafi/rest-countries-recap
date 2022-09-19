5d8babb606ed6d53a2cbe1e302be222d51bbcbde
S M SHAFI <asshafi15@gmail.com>
2022-09-19 17:34:26 +0600

    second commit

--- js/countries.js
+++ js/countries.js
@@ -9,15 +9,37 @@ const displayCountries = countries => {
     const container = document.getElementById("countries");
     container.innerHTML = allCountriesHTML.join(' ');
 }
-
-const getCountryHTML = country => {
+const getCountryHTML = ({ name, flags,area ,region }) => {
     return `
     <div class="country">
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.png}">
+        <h2>${name.common}</h2>
+        <p> Area: ${area}</p>
+        <p>Continet: ${region}</p>
+        <img src="${flags.png}">
     </div>
     `
 }
+
+// const getCountryHTML = (country) => {
+//     // step: 1
+//     const { name, flags } = country;
+//     return `
+//     <div class="country">
+//         <h2>${name.common}</h2>
+//         <img src="${flags.png}">
+//     </div>
+//     `
+// }
+
+//original 
+// const getCountryHTML = country => {
+//     return `
+//     <div class="country">
+//         <h2>${country.name.common}</h2>
+//         <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
 
 
 loadCountries();
